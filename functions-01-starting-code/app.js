0e43a8db64286cfe25aed83efd58e0c97ac586eb
Luke Mico <lukemico@me.com>
2020-06-06 18:01:29 +1000

    introduce the Arrow function

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -7,7 +7,7 @@ const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 const RESULT_DRAW = 'DRAW';
-const RESUlT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
 const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 // set the State of the game to false prior to page load.
@@ -38,20 +38,40 @@ const getComputerChoice = function () {
 	}
 };
 
-// declare a function using logic to determine the winner of the game
-const getWinner = function (cChoice, pChoice) {
-	if (cChoice === pChoice) {
-		return RESULT_DRAW;
-	} else if (
-		(cChoice === ROCK && pChoice === PAPER) ||
-		(cChoice === PAPER && pChoice === SCISSORS) ||
-		(cChoice === SCISSORS && pChoice === ROCK)
-	) {
-		return RESUlT_PLAYER_WINS;
-	} else {
-		return RESULT_COMPUTER_WINS;
-	}
+// declare an Arrow function + ternary expression logic to determine the winner of the game
+const getWinner = (cChoice, pChoice) => {
+	return cChoice === pChoice
+		? RESULT_DRAW
+		: (cChoice === ROCK && pChoice === PAPER) ||
+		  (cChoice === PAPER && pChoice === SCISSORS) ||
+		  (cChoice === SCISSORS && pChoice === ROCK)
+		? RESULT_PLAYER_WINS
+		: RESULT_COMPUTER_WINS;
 };
+
+const getWinner = (cChoice, pChoice) =>
+	cChoice === pChoice
+		? RESULT_DRAW
+		: (cChoice === ROCK && pChoice === PAPER) ||
+		  (cChoice === PAPER && pChoice === SCISSORS) ||
+		  (cChoice === SCISSORS && pChoice === ROCK)
+		? RESULT_PLAYER_WINS
+		: RESULT_COMPUTER_WINS;
+
+// declare a function using logic to determine the winner of the game
+// const getWinner = function(cChoice, pChoice) {
+// 	if (cChoice === pChoice) {
+// 		return RESULT_DRAW;
+// 	} else if (
+// 		(cChoice === ROCK && pChoice === PAPER) ||
+// 		(cChoice === PAPER && pChoice === SCISSORS) ||
+// 		(cChoice === SCISSORS && pChoice === ROCK)
+// 	) {
+// 		return RESUlT_PLAYER_WINS;
+// 	} else {
+// 		return RESULT_COMPUTER_WINS;
+// 	}
+// };
 
 // declare an anonymous function that listens for a click event and returns outputs using variables below
 startGameBtn.addEventListener('click', function () {
