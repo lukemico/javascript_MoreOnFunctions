f4226a3640280c61f76ce648c8d052d39872a77b
Luke Mico <lukemico@me.com>
2020-06-08 21:41:18 +1000

    implement Default arguments on functions

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -123,3 +123,16 @@ startGameBtn.addEventListener('click', () => {
 	alert(message);
 	gameIsRunning = false;
 });
+
+// not related to game
+
+const sumUp = (a, b, ...numbers) => {
+	let sum = 0;
+	for (const num of numbers) {
+		sum += num;
+	}
+	return sum;
+};
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
